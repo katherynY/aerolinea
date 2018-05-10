11d86009c57d4cd7814542e337285b9a39501b34
siegh4rt <guzmanchristiaan@outlook.com>
2018-05-10 16:23:42 -0500

    front-componente-floatform

---
+++ aerolinea-front/app/components/FloatForm/index.js
@@ -0,0 +1,38 @@
+/**
+*
+* FloatForm
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+
+class FloatForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div class="FloatForm">
+        <div class="col-md-9">
+          <div class="row">]
+          
+          </div>
+          <div class="row">]
+          <label>fdbdfbdfbd</label>
+          </div>
+          <div class="row">]
+            <label>fdbdfbdfbd</label>
+ 
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+FloatForm.propTypes = {
+
+};
+
+export default FloatForm;
---
+++ aerolinea-front/app/components/FloatForm/messages.js
@@ -0,0 +1,13 @@
+/*
+ * FloatForm Messages
+ *
+ * This contains all the text for the FloatForm component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.FloatForm.header',
+    defaultMessage: 'This is the FloatForm component !',
+  },
+});
